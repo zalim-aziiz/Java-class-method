f577fcab3353c221e98aa371a858a0e6ef5fa60d
zalim-aziiz <125902570+zalim-aziiz@users.noreply.github.com>
2023-02-20 19:29:31 +0300

    Add files via upload
    My  first commit

---
+++ javaclassmethod/src/Main.java
@@ -0,0 +1,12 @@
+Write a Java method to find the smallest number among three numbers.\
+        Test Data:
+        Input the first number: 25
+        Input the Second number: 37
+        Input the third number: 29
+        Expected Output:
+        [19:11, 23/01/2023] MoHa: 2. Write a Java method to compute the average of three numbers.
+        Test Data:
+        Input the first number: 25
+        Input the second number: 45
+        Input the third number: 65
+        Expected Output:
